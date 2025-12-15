72606b36d65d21caafbed014f1a68f7ca86b3cb2
iamlivelikedynamic <dhirendrakj714@gmail.com>
2025-12-15 19:33:18 +0530

    define and use of "typeof" operator

--- 01_Basics/dataTypes_Summary.js
+++ 01_Basics/dataTypes_Summary.js
@@ -14,7 +14,7 @@ const anotherId = Symbol('123')
 
 console.log(id === anotherId);
 
-// const bigNumber = 3456543576654356754n
+const bigNumber = 3456543576654356754n;
 
 
 
@@ -28,10 +28,28 @@ let myObj = {
     age: 22,
 }
 
-const myFunction = function(){
+const myFunction = function () {
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+
+// typeof is an operator that returns a string indicating the type of the unevaluated operand.
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/typeof
+
+// Log the types
+console.log(typeof score);              // "number"
+console.log(typeof scoreValue);        // "number"
+
+console.log(typeof isLoggedIn);       // "boolean"
+console.log(typeof outsideTemp);     // "object"
+console.log(typeof userEmail);      // "undefined"
+
+console.log(typeof id);             //  "symbol"
+console.log(typeof anotherId);     //  "symbol"
+console.log(typeof bigNumber);    // "bigint"
+console.log(typeof heros);      // "object"
+console.log(typeof myObj);     // "object"
+console.log(typeof myFunction);   // "function"
+
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
