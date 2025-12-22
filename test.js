184631c031a3394513b386287943a8d51445eb19
iamlivelikedynamic <dhirendrakj714@gmail.com>
2025-12-22 11:11:58 +0530

    add info about the author

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 console.log("Welcome to the JavaScript World!")
+console.log("The Author of this File : DHIRENDRA JHA");
+
 console.log("Hello!, This Side Dhirendra Jha, ")
 console.log("You are in the journey of learning JavaScript. ")
 console.log("Happy Coding! 😊   ")
