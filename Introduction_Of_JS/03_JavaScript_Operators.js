d0acd28d1d090b45d410284687dd8899b70ee919
iamlivelikedynamic <dhirendrakj714@gmail.com>
2025-12-30 19:39:42 +0530

    Correct the name of the file

---
+++ Introduction_Of_JS/03_JavaScript_Operators.js
@@ -0,0 +1,21 @@
+// JavaScript Operators
+
+/*
+
+JavaScript operators are symbols or keywords used to perform operations on values and variables. They are the building blocks of JavaScript expressions and can manipulate data in various ways.
+
+There are various operators supported by JavaScript:
+
+1. JavaScript Arithmetic Operators:-->
+Arithmetic Operators perform mathematical calculations like addition, subtraction, multiplication, etc.
+
+const sum = 5 + 3; // Addition
+const diff = 10 - 2; // Subtraction
+const p = 4 * 2; // Multiplication
+const q = 8 / 2; // Division
+console.log(sum, diff, p, q);
+
+
+Output: 
+8 8 8 4
+
