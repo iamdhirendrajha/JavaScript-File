38594244f6da71900ebee2445e7a29322d51fc47
iamlivelikedynamic <dhirendrakj714@gmail.com>
2025-12-31 19:24:44 +0530

    add a new file_Type_Coercion.js

---
+++ Introduction_Of_JS/05_Type_Coercion.js
@@ -0,0 +1,28 @@
+// What is Type Coercion in JavaScript?
+
+//JavaScript is a loosely typed or dynamic language, which means it automatically converts types when necessary, making it easier for developers to work with different data types in expressions, comparisons, and operations. However, this flexibility can sometimes lead to unexpected results if you're not careful.
+
+/*
+Type Coercion in JavaScript:--->
+
+In JavaScript, Type Coercion happens when JavaScript automatically changes one type of value into another. Sometimes, this automatic conversion can cause unexpected results if you’re not aware of how it works.
+
+
+Automatic Conversion: When required, the JavaScript automatically converts required data types.
+Works with Three Types: String, Number, and Boolean coercion.
+Can Lead to Unexpected Results: If they are not handled properly, they may cause unintended bugs.
+Implicitly Occurs: Automatically converts the type of the value from one to another.
+
+
+console.log(5 + "10");
+
+Output:
+"510"
+
+In this example:
+JavaScript converts the number 5 into a string.
+Concatenates it with "10".
+
+
+
+
