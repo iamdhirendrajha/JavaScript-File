250b200a37594337aeb2bfb323379a845a87c749
iamlivelikedynamic <dhirendrakj714@gmail.com>
2026-01-01 21:47:50 +0530

    add a new file and some data , data about if-else

--- Introduction_Of_JS/07_if-else.js
+++ Introduction_Of_JS/07_if-else.js
@@ -6,8 +6,21 @@
 JavaScript if-statement:--->
 It is a conditional statement that determines whether a specific action or block of code will run based on a condition. If the condition is true, the code executes; if false, it does not.
 
-Syntax
-
+Syntax:=
 if (condition) {
-    // Code to be executed if the condition is true
-}
+    // Code to be executed if the condition is True                
+    }
+// this block or this line of Code to be executed if the condition is Falae                
+
+Now let's understand this with the help of an example:
+// JavaScript program to illustrate If statement
+
+let age = 19;
+
+if (age > 18)
+console.log("Congratulations, You are eligible to drive");
+
+Output:
+Congratulations, You are eligible to drive
+
+
