5a9b81ce17f3485372a78f7235cc5e1dbe02a7e5
iamlivelikedynamic <dhirendrakj714@gmail.com>
2026-01-01 21:40:13 +0530

    add a m=new file

---
+++ Introduction_Of_JS/07_if-else.js
@@ -0,0 +1,13 @@
+// JavaScript if-else
+
+// JavaScript conditional statements allow programs to make decisions based on specific conditions. They control the flow of execution, enabling different actions for different scenarios.
+
+/*
+JavaScript if-statement:--->
+It is a conditional statement that determines whether a specific action or block of code will run based on a condition. If the condition is true, the code executes; if false, it does not.
+
+Syntax
+
+if (condition) {
+    // Code to be executed if the condition is true
+}
