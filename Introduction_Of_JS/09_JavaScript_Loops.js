0f0043d4e5bec906ad8fb12d910b8b85df17190d
iamlivelikedynamic <dhirendrakj714@gmail.com>
2026-01-03 21:38:40 +0530

    loops completed

--- Introduction_Of_JS/09_JavaScript_Loops.js
+++ Introduction_Of_JS/09_JavaScript_Loops.js
@@ -122,3 +122,14 @@ Syntax:--
 do {
     // Code to execute
 } while (condition);
+
+
+*   do while loop starts with the execution of the statement. There is no checking of any condition for the first time.
+
+*   After the execution of the statements, and update of the variable value, the condition is checked for true or false value. If it is evaluated to true, next iteration of loop starts.
+
+*   When the condition becomes false, the loop terminates which marks the end of its life cycle.
+
+*   It is important to note that the do-while loop will execute its statements a tleast once before any condition is checked, and therefore is an example of exit control loop.
+
+
