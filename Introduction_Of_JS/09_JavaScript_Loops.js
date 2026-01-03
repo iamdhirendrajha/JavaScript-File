0dc60c52111807092b6fc6d68492eb21f90d642e
iamlivelikedynamic <dhirendrakj714@gmail.com>
2026-01-03 21:15:25 +0530

    first loop

---
+++ Introduction_Of_JS/09_JavaScript_Loops.js
@@ -0,0 +1,15 @@
+// JavaScript Loops
+/*
+
+Loops in JavaScript are used to reduce repetitive tasks by repeatedly executing a block of code as long as a specified condition is true. This makes code more concise and efficient.
+
+In JavaScript, there are three types of Loops :
+
+1. for Loop:-->
+The for loop repeats a block of code a specific number of times. It contains initialization, condition, and increment/decrement in one line.
+
+Example: The below JavaScript program for loop runs from i = 1 to i = 3, incrementing i by 1 each time, and prints "Count:" followed by the current value of i.
+
+for (let i = 1; i <= 3; i++) {
+    console.log("Count:", i);
+}
